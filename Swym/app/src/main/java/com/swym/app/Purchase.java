f3306171738f19d551a4aa5e3144acd689b01f8d
shankera <pnarjunshanker@gmail.com>
2014-07-19 23:27:13 -0500

    major ui changes

---
+++ Swym/app/src/main/java/com/swym/app/Purchase.java
@@ -0,0 +1,36 @@
+package com.swym.app;
+
+/**
+ * Created by Arjun Shanker on 7/19/14.
+ */
+public class Purchase {
+    private String name;
+    private double cost;
+    private String description;
+    private String tag;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setCost(double cost){
+        this.cost = cost;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public void setTag(String tag){
+        this.tag = tag;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getCost(){
+        return cost;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public String getTag(){
+        return tag;
+    }
+}
